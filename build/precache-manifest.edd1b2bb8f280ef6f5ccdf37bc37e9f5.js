27b592f2d7a3ca7ec936e45218c8382d462f5f51
Ghouse Mohamed <ghousemohamed7126@gmail.com>
2020-01-14 09:36:32 +0300

    Adding files...removing

---
+++ build/precache-manifest.edd1b2bb8f280ef6f5ccdf37bc37e9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49623b563577baca9cd1da0b1a05a78",
+    "url": "/Understanding-Redux-Saga/index.html"
+  },
+  {
+    "revision": "64e081d700e661c1193f",
+    "url": "/Understanding-Redux-Saga/static/css/main.0841ac80.chunk.css"
+  },
+  {
+    "revision": "e734ce55447624fed87a",
+    "url": "/Understanding-Redux-Saga/static/js/2.47f554a2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Understanding-Redux-Saga/static/js/2.47f554a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64e081d700e661c1193f",
+    "url": "/Understanding-Redux-Saga/static/js/main.3d8ca13d.chunk.js"
+  },
+  {
+    "revision": "c03c183c348df85d68cb",
+    "url": "/Understanding-Redux-Saga/static/js/runtime-main.6244c3e3.js"
+  }
+]);
